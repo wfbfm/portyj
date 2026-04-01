de5a85d901aa148a49fcd53ee9a9f308a3c3933b
wfbfm <wfbfmicou@gmail.com>
2026-04-01 23:25:21 +0100

    Temporarily enable cors

--- src/main/java/com/wfbfm/portyj/config/WebClientConfig.java
+++ src/main/java/com/wfbfm/portyj/config/WebClientConfig.java
@@ -4,15 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer
+{
 
     @Bean
     WebClient webClient() {
         return WebClient.builder()
                 .defaultHeader("User-Agent", "Mozilla/5.0")
                 .build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
     @Bean
--- src/main/java/com/wfbfm/portyj/ui/PositionsController.java
+++ src/main/java/com/wfbfm/portyj/ui/PositionsController.java
@@ -11,10 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -60,6 +57,7 @@ public class PositionsController
         return "fragments/positions-table :: positions-content";
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/api/positions")
     @ResponseBody
     public PositionDto positions()
